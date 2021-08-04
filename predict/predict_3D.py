ed12f1b52de43ca77a7ad8566314e9bbd1a364af
fanhongwei1 <fanhongwei1@sensetime.com>
2021-08-04 20:47:41 +0800

    demo

--- predict/predict_3D.py
+++ predict/predict_3D.py
@@ -12,12 +12,12 @@ from PointRend.point_rend import add_pointrend_config
 from DensePose.densepose import add_densepose_config
 
 import config
+from models.smpl_official import SMPL
 
 from predict.predict_joints2D import predict_joints2D
 from predict.predict_silhouette_pointrend import predict_silhouette_pointrend
 from predict.predict_densepose import predict_densepose
 
-from models.smpl_official import SMPL
 from renderers.weak_perspective_pyrender_renderer import Renderer
 
 from utils.image_utils import pad_to_square, crop_and_resize_silhouette_joints
@@ -77,109 +77,112 @@ def create_proxy_representation(silhouette,
     return proxy_rep
 
 
-def predict_3D(input,
+def predict_3D(video_path,
                regressor,
                device,
                silhouettes_from='densepose',
+               joints2D_predictor=None,
+               silhouette_predictor=None,
+               smpl=None,
                proxy_rep_input_wh=512,
                save_proxy_vis=True,
-               render_vis=True):
-
-    # Set-up proxy representation predictors.
-    joints2D_predictor, silhouette_predictor = setup_detectron2_predictors(silhouettes_from=silhouettes_from)
-
-    # Set-up SMPL model.
-    smpl = SMPL(config.SMPL_MODEL_DIR, batch_size=1).to(device)
+               render_vis=True,
+               output_dir=''):
 
     if render_vis:
         # Set-up renderer for visualisation.
         wp_renderer = Renderer(resolution=(proxy_rep_input_wh, proxy_rep_input_wh))
 
-    if os.path.isdir(input):
-        image_fnames = [f for f in sorted(os.listdir(input)) if f.endswith('.png') or
-                        f.endswith('.jpg')]
-        for fname in tqdm(image_fnames):
-            # print("Predicting on:", fname)
-            image = cv2.imread(os.path.join(input, fname))
-            # Pre-process for 2D detectors
-            image = pad_to_square(image)
-            image = cv2.resize(image, (proxy_rep_input_wh, proxy_rep_input_wh),
-                               interpolation=cv2.INTER_LINEAR)
-            # Predict 2D
-            joints2D, joints2D_vis = predict_joints2D(image, joints2D_predictor)
-            if silhouettes_from == 'pointrend':
-                silhouette, silhouette_vis = predict_silhouette_pointrend(image,
-                                                                          silhouette_predictor)
-            elif silhouettes_from == 'densepose':
-                silhouette, silhouette_vis = predict_densepose(image, silhouette_predictor)
-                silhouette = convert_multiclass_to_binary_labels(silhouette)
-            # Crop around silhouette
-            silhouette, joints2D, image = crop_and_resize_silhouette_joints(silhouette,
-                                                                            joints2D,
-                                                                            out_wh=config.REGRESSOR_IMG_WH,
-                                                                            image=image,
-                                                                            image_out_wh=proxy_rep_input_wh,
-                                                                            bbox_scale_factor=1.2)
-            # Create proxy representation
-            proxy_rep = create_proxy_representation(silhouette, joints2D,
-                                                    out_wh=config.REGRESSOR_IMG_WH)
-            proxy_rep = proxy_rep[None, :, :, :]  # add batch dimension
-            proxy_rep = torch.from_numpy(proxy_rep).float().to(device)
+    cap = cv2.VideoCapture(video_path)
+    video_length = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+    for frame_id in tqdm(range(video_length)[:100], desc='frame'):
+        ret, image = cap.read()
+        
+        if not ret:
+            video_results.append(None)
+            continue
 
-            # Predict 3D
-            regressor.eval()
-            with torch.no_grad():
-                pred_cam_wp, pred_pose, pred_shape = regressor(proxy_rep)
-                # Convert pred pose to rotation matrices
-                if pred_pose.shape[-1] == 24 * 3:
-                    pred_pose_rotmats = batch_rodrigues(pred_pose.contiguous().view(-1, 3))
-                    pred_pose_rotmats = pred_pose_rotmats.view(-1, 24, 3, 3)
-                elif pred_pose.shape[-1] == 24 * 6:
-                    pred_pose_rotmats = rot6d_to_rotmat(pred_pose.contiguous()).view(-1, 24, 3, 3)
+        # Pre-process for 2D detectors
+        image = pad_to_square(image)
+        image = cv2.resize(image, (proxy_rep_input_wh, proxy_rep_input_wh),
+                            interpolation=cv2.INTER_LINEAR)
 
-                pred_smpl_output = smpl(body_pose=pred_pose_rotmats[:, 1:],
-                                        global_orient=pred_pose_rotmats[:, 0].unsqueeze(1),
-                                        betas=pred_shape,
-                                        pose2rot=False)
-                pred_vertices = pred_smpl_output.vertices
-                pred_vertices2d = orthographic_project_torch(pred_vertices, pred_cam_wp)
-                pred_vertices2d = undo_keypoint_normalisation(pred_vertices2d,
-                                                              proxy_rep_input_wh)
+        # Predict 2D
+        joints2D, joints2D_vis = predict_joints2D(image, joints2D_predictor)
+        if silhouettes_from == 'pointrend':
+            silhouette, silhouette_vis = predict_silhouette_pointrend(image,
+                                                                        silhouette_predictor)
+        elif silhouettes_from == 'densepose':
+            silhouette, silhouette_vis = predict_densepose(image, silhouette_predictor)
+            silhouette = convert_multiclass_to_binary_labels(silhouette)
 
-                pred_reposed_smpl_output = smpl(betas=pred_shape)
-                pred_reposed_vertices = pred_reposed_smpl_output.vertices
+        # Crop around silhouette
+        silhouette, joints2D, image = crop_and_resize_silhouette_joints(silhouette,
+                                                                        joints2D,
+                                                                        out_wh=config.REGRESSOR_IMG_WH,
+                                                                        image=image,
+                                                                        image_out_wh=proxy_rep_input_wh,
+                                                                        bbox_scale_factor=1.2)
 
-            # Numpy-fying
-            pred_vertices = pred_vertices.cpu().detach().numpy()[0]
-            pred_vertices2d = pred_vertices2d.cpu().detach().numpy()[0]
-            pred_reposed_vertices = pred_reposed_vertices.cpu().detach().numpy()[0]
-            pred_cam_wp = pred_cam_wp.cpu().detach().numpy()[0]
+        # Create proxy representation
+        proxy_rep = create_proxy_representation(silhouette, joints2D,
+                                                out_wh=config.REGRESSOR_IMG_WH)
+        proxy_rep = proxy_rep[None, :, :, :]  # add batch dimension
+        proxy_rep = torch.from_numpy(proxy_rep).float().to(device)
 
-            if False:
-                if not os.path.isdir(os.path.join(input, 'verts_vis')):
-                    os.makedirs(os.path.join(input, 'verts_vis'))
-                plt.figure()
-                # plt.imshow(image[:,:,::-1])
-                plt.scatter(pred_vertices2d[:, 0], pred_vertices2d[:, 1], s=0.3)
-                plt.gca().set_axis_off()
-                plt.subplots_adjust(top=1, bottom=0, right=1, left=0, hspace=0, wspace=0)
-                plt.margins(0, 0)
-                plt.gca().xaxis.set_major_locator(plt.NullLocator())
-                plt.gca().yaxis.set_major_locator(plt.NullLocator())
-                plt.savefig(os.path.join(input, 'verts_vis', 'verts_'+fname))
+        # Predict 3D
+        regressor.eval()
+        with torch.no_grad():
+            pred_cam_wp, pred_pose, pred_shape = regressor(proxy_rep)
+            # Convert pred pose to rotation matrices
+            if pred_pose.shape[-1] == 24 * 3:
+                pred_pose_rotmats = batch_rodrigues(pred_pose.contiguous().view(-1, 3))
+                pred_pose_rotmats = pred_pose_rotmats.view(-1, 24, 3, 3)
+            elif pred_pose.shape[-1] == 24 * 6:
+                pred_pose_rotmats = rot6d_to_rotmat(pred_pose.contiguous()).view(-1, 24, 3, 3)
 
-                if render_vis:
-                    rend_img = wp_renderer.render(verts=pred_vertices, cam=pred_cam_wp, img=image)
-                    rend_reposed_img = wp_renderer.render(verts=pred_reposed_vertices,
-                                                        cam=np.array([0.8, 0., -0.2]),
-                                                        angle=180,
-                                                        axis=[1, 0, 0])
-                    if not os.path.isdir(os.path.join(input, 'rend_vis')):
-                        os.makedirs(os.path.join(input, 'rend_vis'))
-                    cv2.imwrite(os.path.join(input, 'rend_vis', 'rend_'+fname), rend_img)
-                    cv2.imwrite(os.path.join(input, 'rend_vis', 'reposed_'+fname), rend_reposed_img)
-                if save_proxy_vis:
-                    if not os.path.isdir(os.path.join(input, 'proxy_vis')):
-                        os.makedirs(os.path.join(input, 'proxy_vis'))
-                    cv2.imwrite(os.path.join(input, 'proxy_vis', 'silhouette_'+fname), silhouette_vis)
-                    cv2.imwrite(os.path.join(input, 'proxy_vis', 'joints2D_'+fname), joints2D_vis)
+            pred_smpl_output = smpl(body_pose=pred_pose_rotmats[:, 1:],
+                                    global_orient=pred_pose_rotmats[:, 0].unsqueeze(1),
+                                    betas=pred_shape,
+                                    pose2rot=False)
+            pred_vertices = pred_smpl_output.vertices
+            pred_vertices2d = orthographic_project_torch(pred_vertices, pred_cam_wp)
+            pred_vertices2d = undo_keypoint_normalisation(pred_vertices2d,
+                                                            proxy_rep_input_wh)
+
+            pred_reposed_smpl_output = smpl(betas=pred_shape)
+            pred_reposed_vertices = pred_reposed_smpl_output.vertices
+
+        # Numpy-fying
+        pred_vertices = pred_vertices.cpu().detach().numpy()[0]
+        pred_vertices2d = pred_vertices2d.cpu().detach().numpy()[0]
+        pred_reposed_vertices = pred_reposed_vertices.cpu().detach().numpy()[0]
+        pred_cam_wp = pred_cam_wp.cpu().detach().numpy()[0]
+        
+        # if not os.path.isdir(os.path.join(input, 'verts_vis')):
+        #     os.makedirs(os.path.join(input, 'verts_vis'))
+        # plt.figure()
+        # # plt.imshow(image[:,:,::-1])
+        # plt.scatter(pred_vertices2d[:, 0], pred_vertices2d[:, 1], s=0.3)
+        # plt.gca().set_axis_off()
+        # plt.subplots_adjust(top=1, bottom=0, right=1, left=0, hspace=0, wspace=0)
+        # plt.margins(0, 0)
+        # plt.gca().xaxis.set_major_locator(plt.NullLocator())
+        # plt.gca().yaxis.set_major_locator(plt.NullLocator())
+        # plt.savefig(os.path.join(input, 'verts_vis', 'verts_'+fname))
+
+        if render_vis:
+            rend_img = wp_renderer.render(verts=pred_vertices, cam=pred_cam_wp, img=image)
+            rend_reposed_img = wp_renderer.render(verts=pred_reposed_vertices,
+                                                cam=np.array([0.8, 0., -0.2]),
+                                                angle=180,
+                                                axis=[1, 0, 0])
+            if not os.path.isdir(os.path.join(output_dir, 'rend_vis')):
+                os.makedirs(os.path.join(output_dir, 'rend_vis'))
+            cv2.imwrite(os.path.join(inpuoutput_dirt, 'rend_vis', f'rend_{frame_id}.jpg'), rend_img)
+            cv2.imwrite(os.path.join(output_dir, 'rend_vis', f'reposed_{frame_id}.jpg'), rend_reposed_img)
+        if save_proxy_vis:
+            if not os.path.isdir(os.path.join(output_dir, 'proxy_vis')):
+                os.makedirs(os.path.join(output_dir, 'proxy_vis'))
+            cv2.imwrite(os.path.join(output_dir, 'proxy_vis', f'silhouette_{frame_id}.jpg'), silhouette_vis)
+            cv2.imwrite(os.path.join(output_dir, 'proxy_vis', f'joints2D_{frame_id}.jpg'), joints2D_vis)
--- run_predict.py
+++ run_predict.py
@@ -3,25 +3,51 @@ import argparse
 import torch
 
 from models.regressor import SingleInputRegressor
-from predict.predict_3D import predict_3D
+from models.smpl_official import SMPL
+from predict.predict_3D import predict_3D, setup_detectron2_predictors
+import config
 
-def main(input_path, checkpoint_path, device, silhouettes_from):
+from tqdm import tqdm
+
+def main(input_path, checkpoint_path, device, silhouettes_from, output_dir):
+    
     regressor = SingleInputRegressor(resnet_in_channels=18,
                                      resnet_layers=18,
                                      ief_iters=3)
 
-    print("Regressor loaded. Weights from:", checkpoint_path)
+    # print("Regressor loaded. Weights from:", checkpoint_path)
     regressor.to(device)
     checkpoint = torch.load(checkpoint_path, map_location=device)
     regressor.load_state_dict(checkpoint['best_model_state_dict'])
+    regressor.eval()
 
-    predict_3D(input_path, regressor, device, silhouettes_from=silhouettes_from,
-               save_proxy_vis=False, render_vis=False)
+    # Set-up proxy representation predictors.
+    joints2D_predictor, silhouette_predictor = setup_detectron2_predictors(silhouettes_from=silhouettes_from)
 
+    # Set-up SMPL model.
+    smpl = SMPL(config.SMPL_MODEL_DIR, batch_size=1).to(device)
+
+    os.makedirs(output_dir, exist_ok=True)
+    video_files = os.listdir(input_path)
+    video_paths = [os.path.join(input_path, video_file) for video_file in video_files]
+    for video_path in tqdm(video_paths, desc='video'):
+        basename = os.path.basename(video_path)
+        videoname = basename.split(".")[0]
+        predict_3D(video_path, 
+                    regressor, 
+                    device, 
+                    silhouettes_from=silhouettes_from, 
+                    joints2D_predictor=joints2D_predictor, 
+                    silhouette_predictor=silhouette_predictor,
+                    smpl=smpl,
+                    save_proxy_vis=True, 
+                    render_vis=True, 
+                    output_dir=output_dir)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--input', type=str, help='Path to input image/folder of images.')
+    parser.add_argument('--output', type=str, help='Path to output image/folder of images.')
     parser.add_argument('--checkpoint', type=str, help='Path to model checkpoint')
     parser.add_argument('--silh_from', choices=['densepose', 'pointrend'])
     parser.add_argument('--gpu', default='0')
@@ -39,4 +65,4 @@ if __name__ == '__main__':
     # If this still doesn't work, just disable rendering visualisation by setting render_vis
     # argument in predict_3D to False.
 
-    main(args.input, args.checkpoint, device, args.silh_from)
+    main(args.input, args.checkpoint, device, args.silh_from, args.output)
